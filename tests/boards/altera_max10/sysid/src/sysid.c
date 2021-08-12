09a755fbbe1f2b45ec00f95c2092e1ad68630eb1
Stephan Linz <linz@li-pro.net>
2021-08-12 11:16:23 +0200

    tests: altera_max10: convert sysid to devicetree
    Convert the sysid test case of board altera_max10 to be
    devicetree based. It takes the Altera Avalon System ID
    base address, the expected system unique identifier
    and timestamp from the new node, labeled sysid in the
    devicetree, which is now mandatory for nios2f.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- tests/boards/altera_max10/sysid/src/sysid.c
+++ tests/boards/altera_max10/sysid/src/sysid.c
@@ -4,16 +4,48 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#define DT_DRV_COMPAT altr_sysid
+
 #include <zephyr.h>
 #include <ztest.h>
+#include <device.h>
 
-#include "altera_avalon_sysid.h"
+#include "altera_avalon_sysid_regs.h"
+
+#define SYSID_NODE		DT_NODELABEL(sysid)
+
+BUILD_ASSERT(DT_NODE_HAS_COMPAT_STATUS(SYSID_NODE, altr_sysid, okay),
+		"Configured system ID is not compatible with this sourcecode "
+		"or disabled or has no node label sysid in the device tree.");
+
+#undef SYSID_BASE
+#undef SYSID_ID
+#undef SYSID_TIMESTAMP
+
+#define SYSID_BASE		DT_REG_ADDR(SYSID_NODE)
+#define SYSID_ID		DT_PROP(SYSID_NODE, system_id)
+#define SYSID_TIMESTAMP		DT_PROP(SYSID_NODE, system_ts)
+
+static int32_t altera_avalon_sysid_test(void)
+{
+	int32_t hwid = IORD_ALTERA_AVALON_SYSID_ID(SYSID_BASE);
+	int32_t hwts = IORD_ALTERA_AVALON_SYSID_TIMESTAMP(SYSID_BASE);
+
+	if ((SYSID_TIMESTAMP == hwts) && (SYSID_ID == hwid))
+		return 0;
+
+	if (hwts - SYSID_TIMESTAMP > 0)
+		return 1;
+
+	else
+		return -1;
+}
 
 void test_sysid(void)
 {
 	int32_t sysid, status = TC_FAIL;
 
-	sysid = alt_avalon_sysid_test();
+	sysid = altera_avalon_sysid_test();
 	if (!sysid) {
 		status = TC_PASS;
 		TC_PRINT("[SysID] hardware and software appear to be in sync\n");

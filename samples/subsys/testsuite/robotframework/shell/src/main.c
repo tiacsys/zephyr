c659b30ec0c9c1554594d847322735e624d58e16
Stefan Kraus <stefan.kraus@ul.com>
2023-12-11 14:04:52 +0100

    samples: Add robotframework example testsuite
    This commit adds an example testsuite for the robotframework
    harness, which runs without renode but on board.
    
    Signed-off-by: Stefan Kraus <stefan.kraus@ul.com>

---
+++ samples/subsys/testsuite/robotframework/shell/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024 Carl Zeiss Meditec AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int main(void)
+{
+	/* Shell application source code is injected by applied Kconfg SHELL
+	 * options, no more "extra" functionalities are required for exemplary
+	 * robot test.
+	 */
+	return 0;
+}

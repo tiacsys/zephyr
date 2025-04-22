b530251b6b8ff743c8b4905236f8548804332dfc
Anas Nashif <anas.nashif@intel.com>
2025-04-22 08:25:55 -0400

    tests: kernel: interrupt: improve doxygen docs

--- tests/kernel/gen_isr_table/src/multilevel_irq.c
+++ tests/kernel/gen_isr_table/src/multilevel_irq.c
@@ -66,6 +66,11 @@ static void test_multi_level_bit_masks_fn(uint32_t irq1, uint32_t irq2, uint32_t
 	}
 }
 
+/**
+ * @brief Test the IRQ number conversion functions for level 1
+ *
+ * @ingroup kernel_interrupt_tests
+ */
 ZTEST(gen_isr_table_multilevel, test_multi_level_bit_masks_l1)
 {
 	uint32_t irq1;
@@ -83,6 +88,11 @@ ZTEST(gen_isr_table_multilevel, test_multi_level_bit_masks_l1)
 	test_multi_level_bit_masks_fn(irq1, 0, 0);
 }
 
+/**
+ * @brief Test the IRQ number conversion functions for level 2
+ *
+ * @ingroup kernel_interrupt_tests
+ */
 ZTEST(gen_isr_table_multilevel, test_multi_level_bit_masks_l2)
 {
 	if (!IS_ENABLED(CONFIG_2ND_LEVEL_INTERRUPTS)) {
@@ -108,6 +118,11 @@ ZTEST(gen_isr_table_multilevel, test_multi_level_bit_masks_l2)
 	test_multi_level_bit_masks_fn(irq1, irq2, 0);
 }
 
+/**
+ * @brief Test the IRQ number conversion functions for level 3
+ *
+ * @ingroup kernel_interrupt_tests
+ */
 ZTEST(gen_isr_table_multilevel, test_multi_level_bit_masks_l3)
 {
 	if (!IS_ENABLED(CONFIG_3RD_LEVEL_INTERRUPTS)) {

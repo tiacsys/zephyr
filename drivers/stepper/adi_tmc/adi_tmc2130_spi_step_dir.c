d869e96f0848676e23669df941a6073de11d982d
Jan Behrens <jan.behrens@navimatix.de>
2025-05-07 09:12:01 +0200

    First functional driver version

--- drivers/stepper/adi_tmc/adi_tmc2130_spi_step_dir.c
+++ drivers/stepper/adi_tmc/adi_tmc2130_spi_step_dir.c
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "stepper/adi_tmc/adi_tmc_reg.h"
+#include "adi_tmc_reg.h"
 #define DT_DRV_COMPAT adi_tmc2130_spi_step_dir
 
 #include <zephyr/kernel.h>
@@ -81,6 +81,37 @@ static uint8_t tmc2130_spi_sd_ms_res_translator(int32_t ms_res)
 
 static int tmc2130_spi_sd_stepper_enable(const struct device *dev, bool enable)
 {
+	const struct tmc2130_spi_sd_config *config = dev->config;
+	struct tmc2130_spi_sd_data *data = dev->data;
+	int ret;
+
+	/* Check availability of sleep and enable pins, as these might be hardwired. */
+	if (config->en_pin.port == NULL) {
+		LOG_ERR("%s: Failed to enable/disable device, enable pin is not "
+			"available. The device is always on.",
+			dev->name);
+		return -ENOTSUP;
+	}
+
+	if (enable) {
+		ret = gpio_pin_set_dt(&config->en_pin, 1);
+		if (ret != 0) {
+			LOG_ERR("%s: Failed to set en_pin (error: %d)", dev->name, ret);
+			return ret;
+		}
+	} else {
+		ret = gpio_pin_set_dt(&config->en_pin, 0);
+		if (ret != 0) {
+			LOG_ERR("%s: Failed to set en_pin (error: %d)", dev->name, ret);
+			return ret;
+		}
+	}
+	data->enabled = enable;
+
+	if (!enable) {
+		step_dir_stepper_common_accel_stop(dev);
+		gpio_pin_set_dt(&config->common.step_pin, 0);
+	}
 
 	return 0;
 }
@@ -89,6 +120,68 @@ static int
 tmc2130_spi_sd_stepper_set_micro_step_res(const struct device *dev,
 					  enum stepper_micro_step_resolution micro_step_res)
 {
+	const struct tmc2130_spi_sd_config *config = dev->config;
+	struct tmc2130_spi_sd_data *data = dev->data;
+	int ret;
+
+	uint8_t ustep_res_reg_value = tmc2130_spi_sd_ms_res_translator(micro_step_res);
+
+	if (ustep_res_reg_value == 0xFF) {
+		return -EINVAL;
+	}
+	uint8_t tx_buffer1[5] = {TMC2130_CHOPCONF, 0x00, 0x00, 0x00, 0x00};
+	uint8_t rx_buffer[5];
+
+	struct spi_buf spi_buffer_tx = {
+		.buf = &tx_buffer1,
+		.len = sizeof(tx_buffer1),
+	};
+	struct spi_buf_set spi_buffer_array_tx = {
+		.buffers = &spi_buffer_tx,
+		.count = 1U,
+	};
+
+	struct spi_buf spi_buffer_rx = {
+		.buf = &rx_buffer,
+		.len = sizeof(rx_buffer),
+	};
+	struct spi_buf_set spi_buffer_array_rx = {
+		.buffers = &spi_buffer_rx,
+		.count = 1U,
+	};
+
+	/* Double read, as read value of first command is transmitted on second command. */
+	ret = spi_transceive_dt(&config->spi, &spi_buffer_array_tx, &spi_buffer_array_rx);
+	if (ret != 0) {
+		LOG_ERR("%s: Failed to read register 0x%x (error code: %d)", dev->name,
+			tx_buffer1[0], ret);
+		return ret;
+	}
+	ret = spi_transceive_dt(&config->spi, &spi_buffer_array_tx, &spi_buffer_array_rx);
+	if (ret != 0) {
+		LOG_ERR("%s: Failed to read register 0x%x (error code: %d)", dev->name,
+			tx_buffer1[0], ret);
+		return ret;
+	}
+
+	/* Copy current configuration. */
+	tx_buffer1[2] = rx_buffer[2];
+	tx_buffer1[3] = rx_buffer[3];
+	tx_buffer1[4] = rx_buffer[4];
+
+	/* Set new ustep resolution and enable write. */
+	tx_buffer1[1] = ustep_res_reg_value;
+	tx_buffer1[0] += TMC2130_WRITE_BIT;
+
+	/* Write new ustep resolution. */
+	ret = spi_transceive_dt(&config->spi, &spi_buffer_array_tx, &spi_buffer_array_rx);
+	if (ret != 0) {
+		LOG_ERR("%s: Failed to write register 0x%x (error code: %d)", dev->name,
+			tx_buffer1[0], ret);
+		return ret;
+	}
+
+	data->ustep_res = micro_step_res;
 
 	return 0;
 }
@@ -99,7 +192,44 @@ tmc2130_spi_sd_stepper_get_micro_step_res(const struct device *dev,
 {
 	struct tmc2130_spi_sd_data *data = dev->data;
 	*micro_step_res = data->ustep_res;
+	// TODO: maybe read from register?
 	return 0;
+}
+
+static int tmc2130_spi_sd_stepper_move_to(const struct device *dev, int32_t target)
+{
+	struct tmc2130_spi_sd_data *data = dev->data;
+
+	if (!data->enabled) {
+		LOG_ERR("Failed to move to target position, device is not enabled");
+		return -ECANCELED;
+	}
+
+	return step_dir_stepper_common_accel_move_to(dev, target);
+}
+
+static int tmc2130_spi_sd_stepper_move_by(const struct device *dev, int32_t steps)
+{
+	struct tmc2130_spi_sd_data *data = dev->data;
+
+	if (!data->enabled) {
+		LOG_ERR("Failed to move by delta, device is not enabled");
+		return -ECANCELED;
+	}
+
+	return step_dir_stepper_common_accel_move_by(dev, steps);
+}
+
+static int tmc2130_spi_sd_stepper_run(const struct device *dev, enum stepper_direction direction)
+{
+	struct tmc2130_spi_sd_data *data = dev->data;
+
+	if (!data->enabled) {
+		LOG_ERR("Failed to run stepper, device is not enabled");
+		return -ECANCELED;
+	}
+
+	return step_dir_stepper_common_accel_run(dev, direction);
 }
 
 static int tmc2130_spi_sd_stepper_init(const struct device *dev)
@@ -108,8 +238,8 @@ static int tmc2130_spi_sd_stepper_init(const struct device *dev)
 	struct tmc2130_spi_sd_data *data = dev->data;
 
 	uint8_t ustep_res_reg_value = tmc2130_spi_sd_ms_res_translator(data->ustep_res);
-	
-	//TODO: Replace some values with DT entries, and maybe move some parameters to config.
+
+	// TODO: Replace some values with DT entries, and maybe move some parameters to config.
 
 	uint8_t tx_buffer1[5] = {
 		TMC2130_WRITE_BIT + TMC2130_CHOPCONF, ustep_res_reg_value, 0x01, 0x00,
@@ -151,37 +281,62 @@ static int tmc2130_spi_sd_stepper_init(const struct device *dev)
 
 	int ret = spi_transceive_dt(&config->spi, &spi_buffer_array_tx, &spi_buffer_array_rx);
 	if (ret != 0) {
-		LOG_ERR("Failed to write register 0x%x (error code: %d)", tx_buffer1[0], ret);
+		LOG_ERR("%s: Failed to write register 0x%x (error code: %d)", dev->name,
+			tx_buffer1[0], ret);
 		return ret;
 	}
 	spi_buffer_tx.buf = &tx_buffer2;
 	ret = spi_transceive_dt(&config->spi, &spi_buffer_array_tx, &spi_buffer_array_rx);
 	if (ret != 0) {
-		LOG_ERR("Failed to write register 0x%x (error code: %d)", tx_buffer2[0], ret);
+		LOG_ERR("%s: Failed to write register 0x%x (error code: %d)", dev->name,
+			tx_buffer2[0], ret);
 		return ret;
 	}
 	spi_buffer_tx.buf = &tx_buffer3;
 	ret = spi_transceive_dt(&config->spi, &spi_buffer_array_tx, &spi_buffer_array_rx);
 	if (ret != 0) {
-		LOG_ERR("Failed to write register 0x%x (error code: %d)", tx_buffer3[0], ret);
+		LOG_ERR("%s: Failed to write register 0x%x (error code: %d)", dev->name,
+			tx_buffer3[0], ret);
 		return ret;
 	}
 	spi_buffer_tx.buf = &tx_buffer4;
 	ret = spi_transceive_dt(&config->spi, &spi_buffer_array_tx, &spi_buffer_array_rx);
 	if (ret != 0) {
-		LOG_ERR("Failed to write register 0x%x (error code: %d)", tx_buffer4[0], ret);
+		LOG_ERR("%s: Failed to write register 0x%x (error code: %d)", dev->name,
+			tx_buffer4[0], ret);
 		return ret;
 	}
 	spi_buffer_tx.buf = &tx_buffer5;
 	ret = spi_transceive_dt(&config->spi, &spi_buffer_array_tx, &spi_buffer_array_rx);
 	if (ret != 0) {
-		LOG_ERR("Failed to write register 0x%x (error code: %d)", tx_buffer5[0], ret);
+		LOG_ERR("%s: Failed to write register 0x%x (error code: %d)", dev->name,
+			tx_buffer5[0], ret);
 		return ret;
 	}
 	spi_buffer_tx.buf = &tx_buffer6;
 	ret = spi_transceive_dt(&config->spi, &spi_buffer_array_tx, &spi_buffer_array_rx);
 	if (ret != 0) {
-		LOG_ERR("Failed to write register 0x%x (error code: %d)", tx_buffer6[0], ret);
+		LOG_ERR("%s: Failed to write register 0x%x (error code: %d)", dev->name,
+			tx_buffer6[0], ret);
+		return ret;
+	}
+
+	/* Configure enable pin if it is available */
+	if (config->en_pin.port != NULL) {
+		ret = gpio_pin_configure_dt(&config->en_pin, GPIO_OUTPUT_INACTIVE);
+		if (ret != 0) {
+			LOG_ERR("%s: Failed to configure en_pin (error: %d)", dev->name, ret);
+			return ret;
+		}
+		// data->pin_states.en = 0U;
+	} else {
+		data->enabled = true;
+	}
+
+	ret = step_dir_stepper_common_accel_init(dev);
+	if (ret != 0) {
+		LOG_ERR("%s: Failed to initialize common step direction stepper (error: %d)",
+			dev->name, ret);
 		return ret;
 	}
 
@@ -190,13 +345,13 @@ static int tmc2130_spi_sd_stepper_init(const struct device *dev)
 
 static DEVICE_API(stepper, tmc2130_spi_sd_stepper_api) = {
 	.enable = tmc2130_spi_sd_stepper_enable,
-	.move_by = step_dir_stepper_common_accel_move_by,
+	.move_by = tmc2130_spi_sd_stepper_move_by,
 	.is_moving = step_dir_stepper_common_accel_is_moving,
 	.set_reference_position = step_dir_stepper_common_accel_set_reference_position,
 	.get_actual_position = step_dir_stepper_common_accel_get_actual_position,
-	.move_to = step_dir_stepper_common_accel_move_to,
+	.move_to = tmc2130_spi_sd_stepper_move_to,
 	.set_microstep_interval = step_dir_stepper_common_accel_set_microstep_interval,
-	.run = step_dir_stepper_common_accel_run,
+	.run = tmc2130_spi_sd_stepper_run,
 	.set_event_callback = step_dir_stepper_common_accel_set_event_callback,
 	.set_micro_step_res = tmc2130_spi_sd_stepper_set_micro_step_res,
 	.get_micro_step_res = tmc2130_spi_sd_stepper_get_micro_step_res,
@@ -205,7 +360,7 @@ static DEVICE_API(stepper, tmc2130_spi_sd_stepper_api) = {
 #define TMC2130_SPI_SD_STEPPER_DEVICE(inst)                                                        \
 	static const struct tmc2130_spi_sd_config tmc2130_spi_sd_config_##inst = {                 \
 		.common = STEP_DIR_STEPPER_DT_INST_COMMON_ACCEL_CONFIG_INIT(inst),                 \
-		.en_pin = GPIO_DT_SPEC_INST_GET(inst, en_gpios),                                   \
+		.en_pin = GPIO_DT_SPEC_INST_GET_OR(inst, en_gpios, {0}),                           \
 		.spi = SPI_DT_SPEC_INST_GET(inst,                                                  \
 					    (SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB |               \
 					     SPI_MODE_CPOL | SPI_MODE_CPHA | SPI_WORD_SET(8)),     \
